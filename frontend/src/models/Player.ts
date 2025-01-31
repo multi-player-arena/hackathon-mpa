bfaf6d1631da386dd9fdd4360e708717f4ef173d
gdemonteynard <gauthier-externe.de-monteynard@datanumia.com>
2025-01-31 23:21:07 +0100

    Player becomes an interface

--- frontend/src/models/Player.ts
+++ frontend/src/models/Player.ts
@@ -1,8 +1,4 @@
-export class Player {
-  id: number;
-  name: string;
-  constructor(id: number, name: string) {
-    this.id = id;
-    this.name = name;
-  }
+export interface Player {
+    id: number;
+    name: string;
 }
