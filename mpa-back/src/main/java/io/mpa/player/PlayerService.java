64eea72d18c57f5f82ad0cf2ee3bb18f3eeee029
gdemonteynard <gauthier-externe.de-monteynard@datanumia.com>
2025-01-31 22:33:15 +0100

    broadcatsing player

--- mpa-back/src/main/java/io/mpa/player/PlayerService.java
+++ mpa-back/src/main/java/io/mpa/player/PlayerService.java
@@ -1,21 +1,20 @@
 package io.mpa.player;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import io.mpa.ws.TestSocket;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PlayerService {
     private final PlayerDao playerDao;
+    private final TestSocket testSocket;
     private int idIncrement = 0;
-
-    public PlayerService(PlayerDao playerDao) {
-        this.playerDao = playerDao;
-    }
 
     public Player addPlayer(String name) {
         Player player = createPlayer(name);
         playerDao.addPlayer(player);
+        testSocket.broadcastMessage(player);
         return player;
     }
 
--- mpa-back/src/main/java/io/mpa/ws/TestSocket.java
+++ mpa-back/src/main/java/io/mpa/ws/TestSocket.java
@@ -1,25 +1,21 @@
 package io.mpa.ws;
 
+import io.mpa.player.Player;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class TestSocket {
-  private static final Logger LOGGER = LoggerFactory.getLogger(TestSocket.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestSocket.class);
 
-    @Autowired
-    SimpMessagingTemplate messagingTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/broadcast")
-    public void broadcastMessage(@Payload String message) {
-        LOGGER.info("Message received: {}",message);
-//        return "Hello depuis le back";
-        messagingTemplate.convertAndSend("/topic/reply", "You have received a message: " + message);
+    public void broadcastMessage(@Payload Player message) {
+        messagingTemplate.convertAndSend("/topic/reply", message);
     }
 }
