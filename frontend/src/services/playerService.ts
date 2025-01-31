13a76e8c546ef25933ba462294d55d8a1e6b654d
bastien707 <bastien.facqueur@efrei.net>
2025-01-31 23:12:06 +0100

    feat: add cors config

--- frontend/src/services/playerService.ts
+++ frontend/src/services/playerService.ts
@@ -2,7 +2,7 @@ const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export async function createPlayer(name: string) {
     const encodedName = encodeURIComponent(name);
-    const response = fetch(`${apiBaseUrl}/player?${encodedName}` , {
+    const response = fetch(`${apiBaseUrl}/player/${encodedName}` , {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
