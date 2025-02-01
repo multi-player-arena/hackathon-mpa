0987f23b56c02c0f1774c136e9c4e9b2b39cf8bc
ymougenel <ymougenel@takima.fr>
2025-02-01 03:57:18 +0100

    fix front ci?

--- frontend/src/component/GameRender.tsx
+++ frontend/src/component/GameRender.tsx
@@ -1,9 +1,9 @@
 import {MockButton} from "./MockButton.tsx";
 import {PlayerMinigame} from "./PlayerMinigame.tsx";
 import {PublishComponent} from "./PublishComponent.tsx";
-import {usePlayersInGame} from "../providers/PlayersInGameContext.tsx";
-import {useSocketService} from "../services/useSocketService.ts";
-import {Player} from "../models/Player.ts";
+// import {usePlayersInGame} from "../providers/PlayersInGameContext.tsx";
+// import {useSocketService} from "../services/useSocketService.ts";
+// import {Player} from "../models/Player.ts";
 
 export function GameRender() {
     // const {players, addPlayer} = usePlayersInGame()
