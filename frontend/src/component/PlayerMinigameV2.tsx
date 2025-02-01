2ecfd9aaae57eb7433172977b6eabc6108844e64
g.monteynard <gdemonteynard@takima.fr>
2025-02-01 12:10:49 +0100

    feat : create new grid

--- frontend/src/component/PlayerMinigameV2.tsx
+++ frontend/src/component/PlayerMinigameV2.tsx
@@ -2,8 +2,7 @@ import {useSocketService} from "../services/useSocketService.ts";
 import {Action, ActionsEnum, Player} from "../models/Player.ts";
 import {Fragment, useState} from "react";
 import {Graphics, Sprite, Stage, Text} from "@pixi/react";
-import {grid} from "../models/Grid.ts";
-import {Cell} from "./PlayerMinigame.tsx";
+import {Dialog} from "primereact/dialog";
 import {Dialog} from "primereact/dialog";
 import {avatarsMapping} from "./ChooseAvatarComponent.tsx";
 
@@ -20,11 +19,18 @@ interface Wall {
     height: number;
 }
 
-const GAMESIZE_WIDTH = 1400
+interface Wall {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+
+const GAMESIZE_WIDTH = 800
 const GAMESIZE_HEIGHT = 600
 
-const BLOCK_HEIGHT = GAMESIZE_HEIGHT/30
-const BLOCK_WIDTH = GAMESIZE_WIDTH/150
+const BLOCK_HEIGHT = GAMESIZE_HEIGHT / 20
+const BLOCK_WIDTH = GAMESIZE_WIDTH / 33
 
 export function PlayerMinigameV2() {
     const [started, setStarted] = useState<boolean>(false)
@@ -37,75 +43,137 @@ export function PlayerMinigameV2() {
         {x: 0, y: 0, width: GAMESIZE_WIDTH, height: 1},
         {x: 0, y: GAMESIZE_HEIGHT - 1, width: GAMESIZE_WIDTH, height: 1}]
 
+    const [winner, setWinner] = useState<Player | undefined>(undefined)
+    const winZone: Wall = {x: 31 * BLOCK_WIDTH, y: 19 * BLOCK_HEIGHT, width: 200, height: GAMESIZE_HEIGHT}
+
+
     const walls: Wall[] = [{x: 0, y: 0, width: 1, height: GAMESIZE_HEIGHT},
         {x: GAMESIZE_WIDTH - 1, y: 0, width: 1, height: GAMESIZE_HEIGHT},
         {x: 0, y: 0, width: GAMESIZE_WIDTH, height: 1},
         {x: 0, y: GAMESIZE_HEIGHT - 1, width: GAMESIZE_WIDTH, height: 1},
-        {x: 50, y: 50, width: 100, height: 20},
-        {x: 200, y: 50, width: 20, height: 100},
-        {x: 300, y: 50, width: 100, height: 20},
-        {x: 400, y: 100, width: 20, height: 100},
-        {x: 500, y: 150, width: 100, height: 20},
-        {x: 600, y: 150, width: 20, height: 100},
-        {x: 700, y: 200, width: 100, height: 20},
-        {x: 50, y: 250, width: 100, height: 20},
-        {x: 150, y: 250, width: 20, height: 100},
-        {x: 250, y: 250, width: 100, height: 20},
-        {x: 350, y: 300, width: 20, height: 100},
-        {x: 450, y: 300, width: 100, height: 20},
-        {x: 550, y: 350, width: 20, height: 100},
-        {x: 650, y: 350, width: 100, height: 20},
-        {x: 750, y: 400, width: 20, height: 100},
-        {x: 100, y: 450, width: 100, height: 20},
-        {x: 200, y: 450, width: 20, height: 100},
-        {x: 300, y: 450, width: 100, height: 20},
-        {x: 400, y: 500, width: 20, height: 100},
-        {x: 500, y: 500, width: 100, height: 20},
-        {x: 600, y: 550, width: 20, height: 100},
-        {x: 700, y: 550, width: 100, height: 20},
-        {x: 50, y: 600, width: 100, height: 20},
-        {x: 150, y: 600, width: 20, height: 100},
-        {x: 250, y: 600, width: 100, height: 20},
-        {x: 350, y: 50, width: 20, height: 100},
-        {x: 450, y: 50, width: 100, height: 20},
-        {x: 550, y: 100, width: 20, height: 100},
-        {x: 650, y: 100, width: 100, height: 20},
-        {x: 750, y: 150, width: 20, height: 100},
-        {x: 100, y: 200, width: 100, height: 20},
-        {x: 200, y: 200, width: 20, height: 100},
-        {x: 300, y: 200, width: 100, height: 20},
-        {x: 400, y: 250, width: 20, height: 100},
-        {x: 500, y: 250, width: 100, height: 20},
-        {x: 600, y: 300, width: 20, height: 100},
-        {x: 700, y: 300, width: 100, height: 20},
-        {x: 100, y: 350, width: 100, height: 20},
-        {x: 200, y: 350, width: 20, height: 100},
-        {x: 300, y: 350, width: 100, height: 20},
-        {x: 400, y: 400, width: 20, height: 100},
-        {x: 500, y: 400, width: 100, height: 20},
-        {x: 600, y: 450, width: 20, height: 100},
-        {x: 700, y: 450, width: 100, height: 20},
-        {x: 50, y: 500, width: 100, height: 20},
-        {x: 150, y: 500, width: 20, height: 100},
-        {x: 250, y: 500, width: 100, height: 20},
-        {x: 350, y: 550, width: 20, height: 100},
-        {x: 450, y: 550, width: 100, height: 20},
-        {x: 550, y: 600, width: 20, height: 100},
-        {x: 650, y: 600, width: 100, height: 20},
-        // Add more walls to reach 200 walls...
-        {x: 150, y: 50, width: 20, height: 100},
-        {x: 200, y: 150, width: 100, height: 20},
-        {x: 250, y: 200, width: 20, height: 100},
-        {x: 300, y: 300, width: 100, height: 20},
-        {x: 350, y: 350, width: 20, height: 100},
-        {x: 400, y: 400, width: 100, height: 20},
-        {x: 450, y: 450, width: 20, height: 100},
-        {x: 500, y: 500, width: 100, height: 20},
-        {x: 550, y: 550, width: 20, height: 100},
-        {x: 600, y: 600, width: 100, height: 20},
         // More walls here until 200 total
+        {x: 32* BLOCK_WIDTH, y: 3 * BLOCK_HEIGHT, width: BLOCK_WIDTH, height: 15 * BLOCK_HEIGHT},
+        {x: 0, y: 3 * BLOCK_HEIGHT, width: BLOCK_WIDTH, height: 27 * BLOCK_HEIGHT},
+        {x: 0, y: 19 * BLOCK_HEIGHT, width: BLOCK_WIDTH *31, height: BLOCK_HEIGHT},
+        //custom
+        {x: 2 * BLOCK_WIDTH, y: 3 * BLOCK_HEIGHT, width: 11 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 14 * BLOCK_WIDTH, y: 3 * BLOCK_HEIGHT, width: 13 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 28 * BLOCK_WIDTH, y: 3 * BLOCK_HEIGHT, width: 8 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 10 * BLOCK_WIDTH, y: 4 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 16 * BLOCK_WIDTH, y: 4 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 2 * BLOCK_WIDTH, y: 5 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 8 * BLOCK_WIDTH, y: 5 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 14 * BLOCK_WIDTH, y: 5 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 18 * BLOCK_WIDTH, y: 5 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 24 * BLOCK_WIDTH, y: 5 * BLOCK_HEIGHT, width: 7 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 2 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 6 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 8 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 14 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 18 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 22 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 28 * BLOCK_WIDTH, y: 6 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 2 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 4 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 6 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 8 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 10 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 16 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 18 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 22 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        {x: 28 * BLOCK_WIDTH, y: 7 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT},
+        { x: 2 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 10 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 16 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 18 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 20 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 28 * BLOCK_WIDTH, y: 8 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 2 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 10 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 7 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 18 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 20 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 28 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 30 * BLOCK_WIDTH, y: 9 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
 
+        { x: 2 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 14 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 20 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 28 * BLOCK_WIDTH, y: 10 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
 
+        { x: 0 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 10 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 16 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 18 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 20 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 22 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 28 * BLOCK_WIDTH, y: 11 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+
+        { x: 4 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 16 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 18 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 22 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 28 * BLOCK_WIDTH, y: 12 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+
+        { x: 2 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 12 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 16 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 22 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 30 * BLOCK_WIDTH, y: 13 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+
+        { x: 2 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 8 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 10 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 12 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 18 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 30 * BLOCK_WIDTH, y: 14 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 2 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 8 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 10 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 12 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 16 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 7 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 30 * BLOCK_WIDTH, y: 15 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 2 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 8 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 14 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 16 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 20 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 22 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 30 * BLOCK_WIDTH, y: 16 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 2 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 8 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 9 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 18 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 3 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 22 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 26 * BLOCK_WIDTH, y: 17 * BLOCK_HEIGHT, width: 5 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 4 * BLOCK_WIDTH, y: 18 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 6 * BLOCK_WIDTH, y: 18 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT },
+        { x: 24 * BLOCK_WIDTH, y: 18 * BLOCK_HEIGHT, width: 1 * BLOCK_WIDTH, height: BLOCK_HEIGHT }
     ];
 
     const getNextPositionInfo: (info: Infos, action: ActionsEnum) => Infos = (info, action) => {
