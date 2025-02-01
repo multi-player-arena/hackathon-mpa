bfa66bb36c40d8ff9c76c72c99a6d0e8fc231d84
ymougenel <ymougenel@takima.fr>
2025-02-01 04:58:44 +0100

    fix websocket endpoint

--- frontend/src/pages/GamePage.tsx
+++ frontend/src/pages/GamePage.tsx
@@ -1,13 +1,14 @@
 import {StompSessionProvider} from "react-stomp-hooks";
 import {PlayersInGameProvider} from "../providers/PlayersInGameContext.tsx";
 import {GameRender} from "../component/GameRender.tsx";
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export function GamePage() {
 
     return (
         <PlayersInGameProvider>
             <StompSessionProvider
-                url={'http://localhost:8080/ws-endpoint'}>
+                url={`${apiBaseUrl}/ws-endpoint`}>
                 <GameRender/>
 
             </StompSessionProvider>
